5d1f45818942785f577c3c67109be8b37394c789
Ivonaldo soares <ivonaldojrsoares@gmail.com>
2025-01-30 21:20:38 -0300

    adiciona listagem de usuarios

--- listagem.js
+++ listagem.js
@@ -1,6 +1,18 @@
-function getContatos () {
+async function getContatos () {
     const resultHtml = document.getElementById('result');
-    console.log(resultHtml);
+    const apiUrl = 'http://localhost:3000/contatos'
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+
+    data.forEach(usuario => {
+        resultHtml.insertAdjacentHTML('beforeend', `
+            <tr>
+                <td>${usuario.nome}</td>    
+                <td>${usuario.cpf}</td>    
+                <td>${usuario.idade}</td>    
+            </tr>
+        `)
+    });
 }
 
 getContatos();
